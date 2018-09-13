e9abaea4850e7f93b0e1321a3ba9e5bb91fc61e2
NateRex <nrex@icloud.com>
2018-09-12 22:27:05 -0400

    ScenarioBuilder handles adding mob spawners, as well as setting time of day

--- lib/ScenarioBuilder.py
+++ lib/ScenarioBuilder.py
@@ -3,9 +3,9 @@
 # mission. Note: The only class in this file that should be used directly by callers is the
 # ScenarioBuilder
 # ==============================================================================================
-from Constants import Direction
+from Constants import *
 
-class DecorationBuilder:
+class EnvironmentBuilder:
     """
     Internal class used by the ScenarioBuilder for developing XML for the environment of a Malmo mission
     """
@@ -13,31 +13,104 @@ class DecorationBuilder:
     def __init__(self):
         self.__generatorString = "3;7,2*3,2;1;"
         self.__decoratorsXML = ""
+        self.__allowedMobs = set([])
 
-    def addCube(self, point0, point1, blockType):
+    def getAllowedMobsList(self):
+        """
+        Returns a list of strings representing all mobs that are allowed to spawn.
+        """
+        return self.__allowedMobs
+
+    def turnOnAnimalSpawning(self):
+        """
+        Allow for the natural spawning of animals & villagers.
+        """
+        self.__allowedMobs.add(MobType.Pig.value)
+        self.__allowedMobs.add(MobType.Sheep.value)
+        self.__allowedMobs.add(MobType.Cow.value)
+        self.__allowedMobs.add(MobType.Chicken.value)
+        self.__allowedMobs.add(MobType.Ozelot.value)
+        self.__allowedMobs.add(MobType.Rabbit.value)
+        self.__allowedMobs.add(MobType.Villager.value)
+
+    def turnOffAnimalSpawning(self):
+        """
+        Disallow the natural spawning of animals & villagers.
+        """
+        self.__allowedMobs.discard(MobType.Pig.value)
+        self.__allowedMobs.discard(MobType.Sheep.value)
+        self.__allowedMobs.discard(MobType.Cow.value)
+        self.__allowedMobs.discard(MobType.Chicken.value)
+        self.__allowedMobs.discard(MobType.Ozelot.value)
+        self.__allowedMobs.discard(MobType.Rabbit.value)
+        self.__allowedMobs.discard(MobType.Villager.value)
+
+    def turnOnMonsterSpawning(self):
+        """
+        Allow for the natural spawning of monsters.
+        """
+        self.__allowedMobs.add(MobType.Spider.value)
+        self.__allowedMobs.add(MobType.Zombie.value)
+        self.__allowedMobs.add(MobType.Skeleton.value)
+        self.__allowedMobs.add(MobType.Creeper.value)
+
+    def turnOffMonsterSpawning(self):
+        """
+        Disallow for the natural spawning of monsters.
+        """
+        self.__allowedMobs.discard(MobType.Spider.value)
+        self.__allowedMobs.discard(MobType.Zombie.value)
+        self.__allowedMobs.discard(MobType.Skeleton.value)
+        self.__allowedMobs.discard(MobType.Creeper.value)
+
+    def addCube(self, point0, point1, blockType, variant = None):
         """
         Add a cuboid of a specific block type from lower-left-near corner point0 to upper-right-far corner point1.
-        Each point is specified as an (x, y, z) tuple.
+        Each point is specified as an (x, y, z) tuple. If the block type specified is a mob spawner an additional mob type
+        must be provided.
         """
-        self.__decoratorsXML += '''<DrawCuboid x1="{}" y1="{}" z1="{}" x2="{}" y2="{}" z2="{}" type="{}"/>'''.format(point0[0], point0[1], point0[2], point1[0], point1[1], point1[2], blockType.value)
+        if (blockType == BlockType.Mob_spawner):
+            if (variant != None):
+                self.__allowedMobs.add(variant.value)   # Ensure the mob is allowed to spawn
+                self.__decoratorsXML += '''<DrawCuboid x1="{}" y1="{}" z1="{}" x2="{}" y2="{}" z2="{}" type="{}" variant="{}"/>'''.format(point0[0], point0[1], point0[2], point1[0], point1[1], point1[2], blockType.value, variant.value)
+        else:
+            self.__decoratorsXML += '''<DrawCuboid x1="{}" y1="{}" z1="{}" x2="{}" y2="{}" z2="{}" type="{}"/>'''.format(point0[0], point0[1], point0[2], point1[0], point1[1], point1[2], blockType.value)
 
-    def addLine(self, point0, point1, blockType):
+    def addLine(self, point0, point1, blockType, variant = None):
         """
         Add a line of a specific block type from point0 to point1, where each point is specified as an (x, y, z) tuple.
+        If the block type specified is a mob spawner, an additional mob type must be provided.
         """
-        self.__decoratorsXML += '''<DrawLine x1="{}" y1="{}" z1="{}" x2="{}" y2="{}" z2="{}" type="{}"/>'''.format(point0[0], point0[1], point0[2], point1[0], point1[1], point1[2], blockType.value)
+        if (blockType == BlockType.Mob_spawner):
+            if (variant != None):
+                self.__allowedMobs.add(variant.value)   # Ensure the mob is allowed to spawn
+                self.__decoratorsXML += '''<DrawLine x1="{}" y1="{}" z1="{}" x2="{}" y2="{}" z2="{}" type="{}" variant="{}"/>'''.format(point0[0], point0[1], point0[2], point1[0], point1[1], point1[2], blockType.value, variant.value)
+        else:
+            self.__decoratorsXML += '''<DrawLine x1="{}" y1="{}" z1="{}" x2="{}" y2="{}" z2="{}" type="{}"/>'''.format(point0[0], point0[1], point0[2], point1[0], point1[1], point1[2], blockType.value)
 
-    def addBlock(self, location, blockType):
+    def addBlock(self, location, blockType, variant = None):
         """
         Add a block of a specific type at the location specified. The location should be given as an (x, y, z) tuple.
+        If the block type specified is a mob spawner, an additional mob type must be provided.
         """
-        self.__decoratorsXML += '''<DrawBlock x="{}" y="{}" z="{}" type="{}"/>'''.format(location[0], location[1], location[2], blockType.value)
+        if (blockType == BlockType.Mob_spawner):
+            if (variant != None):
+                self.__allowedMobs.add(variant.value)   # Ensure the mob is allowed to spawn
+                self.__decoratorsXML += '''<DrawBlock x="{}" y="{}" z="{}" type="{}" variant="{}"/>'''.format(location[0], location[1], location[2], blockType.value, variant.value)
+        else:
+            self.__decoratorsXML += '''<DrawBlock x="{}" y="{}" z="{}" type="{}"/>'''.format(location[0], location[1], location[2], blockType.value)
 
-    def addSphere(self, center, radius, blockType):
+    def addSphere(self, center, radius, blockType, variant = None):
         """
         Add a sphere of a specific block type, with a given radius and center. The center should be given as an (x, y, z) tuple.
+        If the block type specified is a mob spawner, an additional mob type must be provided.
         """
-        self.__decoratorsXML += '''<DrawSphere x="{}" y="{}" z="{}" radius="{}" type="{}"/>'''.format(center[0], center[1], center[2], radius, blockType.value)
+        if (blockType == BlockType.Mob_spawner):
+            if (variant != None):
+                self.__allowedMobs.add(variant.value)   # Ensure the mob is allowed to spawn
+                self.__decoratorsXML += '''<DrawSphere x="{}" y="{}" z="{}" radius="{}" type="{}" variant="{}"/>'''.format(center[0], center[1], center[2], radius, blockType.value, variant.value)
+        else:
+            self.__decoratorsXML += '''<DrawSphere x="{}" y="{}" z="{}" radius="{}" type="{}"/>'''.format(center[0], center[1], center[2], radius, blockType.value)
 
     def addDropItem(self, location, itemType):
         """
@@ -120,17 +193,36 @@ class AgentBuilder:
 
 class ScenarioBuilder:
     """
-    Builder for creating a new Malmo mission scenario. Decorations and agent attributes are
+    Builder for creating a new Malmo mission scenario. Environment and agent attributes are
     set through members of this object. Every scenario starts with one agent. Upon creating a new ScenarioBuilder, optionally
     specify a name, starting position, and direction for this agent, otherwise, they will default to "Agent", the origin (0, 0, 0), and
     north, respectively.
     """
 
     def __init__(self, description, timeLimit, agentName = "Agent", startPosition = None, startDirection = None):
-        self.description = description
-        self.timeLimit = timeLimit
-        self.decorations = DecorationBuilder()
+        self.__description = description
+        self.__timeLimit = timeLimit
+        self.__timeOfDay = TimeOfDay.Noon.value
+        self.environment = EnvironmentBuilder()
         self.agents = [AgentBuilder(agentName, startPosition, startDirection)]
+
+    def setDescription(self, description):
+        """
+        Set a description for this specific scenario.
+        """
+        self.__description = description
+
+    def setTimeLimit(self, timeLimit):
+        """
+        Set a time limit for the mission to run.
+        """
+        self.__timeLimit = timeLimit
+
+    def setTimeOfDay(self, timeOfDay):
+        """
+        Set the time of day for this scenario.
+        """
+        self.__timeOfDay = timeOfDay.value
 
     def addAgent(self, name, startPosition = None, startDirection = Direction.North):
         """
@@ -144,6 +236,10 @@ class ScenarioBuilder:
         """
         Returns the complete XML string for the current scenario.
         """
+        mobsList = self.environment.getAllowedMobsList()
+        mobsAllowed = ""
+        for mob in mobsList:
+            mobsAllowed = mobsAllowed + mob + " "
         returnValue = '''
         <?xml version="1.0" encoding="UTF-8" standalone="no" ?>
         <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -154,10 +250,12 @@ class ScenarioBuilder:
             <ServerSection>
                     <ServerInitialConditions>
                         <Time>
-                            <StartTime>1000</StartTime>
+                            <StartTime>{}</StartTime>
                             <AllowPassageOfTime>false</AllowPassageOfTime>
                         </Time>
                         <Weather>clear</Weather>
+                        <AllowSpawning>{}</AllowSpawning>
+                        <AllowedMobs>{}</AllowedMobs>
                     </ServerInitialConditions>
             
                 <ServerHandlers>
@@ -166,7 +264,7 @@ class ScenarioBuilder:
                     <ServerQuitWhenAnyAgentFinishes/>
                 </ServerHandlers>
             </ServerSection>
-            '''.format(self.description, self.decorations.finish(), str(self.timeLimit))
+            '''.format(self.__description, self.__timeOfDay, "false" if len(mobsList) == 0 else "true", mobsAllowed, self.environment.finish(), str(self.__timeLimit))
         for i in range(0, len(self.agents)):
             returnValue += self.agents[i].finish()
         return returnValue + "</Mission>"
