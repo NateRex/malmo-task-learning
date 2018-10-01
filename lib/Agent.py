7c83c395c2b0dc10edd519ba64e4e933cd7722d2
NateRex <nrex@icloud.com>
2018-10-01 15:38:49 -0400

    Added Agent.py, a wrapper class for Malmo agents to incorporate actions via method calls with automatic trace logging

---
+++ lib/Agent.py
@@ -0,0 +1,85 @@
+# ==============================================================================================
+# This file contains wrapper classes and functionality relating to a Malmo agent. This combines
+# several primitive commands into larger commands that can be used to acheive more complex tasks.
+# Logging is also handled automatically for each command that creates a corresponding trace.
+# ==============================================================================================
+import MalmoPython
+
+class Agent:
+    """
+    Wrapper class for a Malmo agent for executing complex commands with corresponding logging.
+    To access the Malmo AgentHost object, use the 'host' member.
+    """
+
+    def __init__(self):
+        self.host = MalmoPython.AgentHost()
+
+    def move(self, speed):
+        """
+        Start moving backwards or forwards at a specific speed. Accepted values range from -1 to 1.
+        A speed of 0 stops the agent.
+        """
+        self.host.sendCommand("move {}".format(speed))
+
+    def strafe(self, speed):
+        """
+        Start moving left or right at a specific speed. Accepted values range from -1 to 1.
+        A speed of 0 stops the agent.
+        """
+        self.host.sendCommand("strafe {}".format(speed))
+
+    def look(self, speed):
+        """
+        Start tilting the agent's head up or down at a specific speed. Accepted values range from -1 to 1.
+        A speed of 0 stops the agent.
+        """
+        self.host.sendCommand("pitch {}".format(speed))
+
+    def turn(self, speed):
+        """
+        Start turning to the left or right at a specific speed. Accepted values range from -1 to 1.
+        A speed of 0 stops the agent.
+        """
+        self.host.sendCommand("turn {}".format(speed))
+
+    def startJumping(self):
+        """
+        Start jumping continuously.
+        """
+        self.host.sendCommand("jump 1")
+
+    def stopJumping(self):
+        """
+        Stop jumping.
+        """
+        self.host.sendCommand("jump 0")
+
+    def startCrouching(self):
+        """
+        Start crouching continuously.
+        """
+        self.host.sendCommand("crouch 1")
+
+    def stopCrouching(self):
+        """
+        Stop crouching.
+        """
+        self.host.sendCommand("crouch 0")
+
+    def startAttacking(self):
+        """
+        Start attacking continuously.
+        """
+        self.host.sendCommand("attack 1")
+
+    def startUsingItem(self):
+        """
+        Begin continuously using the item in the currently selected hotbar slot.
+        """
+        self.host.sendCommand("use 1")
+
+    def stopUsingItem(self):
+        """
+        Stop using the item in the currently selected hotbar slot.
+        """
+        self.host.sendCommand("use 0")
