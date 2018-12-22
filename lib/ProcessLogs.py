35ae9e1bbbf21385a1232a4cc2f9f9493d372881
NateRex <nrex@icloud.com>
2018-12-22 12:08:33 -0500

    Started work on post processing of logs - purpose is to fix inconsistencies that arise due to tolerance issues when running each mission

---
+++ lib/ProcessLogs.py
@@ -0,0 +1,5 @@
+# ==============================================================================================
+# This file represents a standalone script for the post-processing of log files. For each file
+# in the logs/ directory, parse the log and resolve any minor issues that have resulted due to
+# tolerance issues while the mission ran.
+# ==============================================================================================
