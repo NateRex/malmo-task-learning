d5f0f9cb0dfffeba2e784957cf22f503d4c78d2c
NateRex <nrex@icloud.com>
2019-01-10 16:26:23 -0500

    HTNAgents can generate a pointer to an action function with a list of arguments for an action given as a string from an HTN plan

--- lib/HTNAgent.py
+++ lib/HTNAgent.py
@@ -12,12 +12,62 @@ class HTNAgent(Agent):
 
     def __init__(self, name):
         super(HTNAgent, self).__init__(name)
+        self.plan = []      # A list of string actions returned by each call to the HTN
 
-    def __selectAction__(self, actionStr):
+    def __mapStringToFunction__(self, actionStr):
         """
-        Given an action to perform as a string with parameters, select the appropriate agent action to perform and return it
-        along with any parameters in a tuple object.
+        Given an action to perform as a string, select the appropriate agent function to perform and return it
+        along with any parameters in a tuple object. Returns None if the action could not be resolved.
         """
-        if actionStr.startswith("!LOOKAT"):
-            # There are several versions of the lookAt command, depending on the entity
-            ### TODO
+        tokens = actionStr.split("-")
+
+        if len(tokens) == 3:    # COMMANDS LOGGED W/ 2 ARGUMENTS ==================================================
+            if tokens[0] == "!LOOKAT":
+                # There are several versions of the lookAt command, depending on the type of entity
+                entityType = "".join([i for i in tokens[2] if not i.isdigit()])
+                if isMob(entityType) or isItem(entityType):
+                    entity = self.getNearbyEntityById(tokens[2])
+                    if entity != None:
+                        return Action(self.lookAtEntity, [entity])
+                else:
+                    agent = HTNAgent.findAgentById(tokens[2])
+                    if agent != None:
+                        return Action(self.lookAtAgent, [agent])
+                return None
+            elif tokens[0] == "!MOVETO":
+                # There are several versions of the moveTo command, depending on the type of entity
+                entityType = "".join([i for i in tokens[2] if not i.isdigit()])
+                if isMob(entityType):
+                    mob = self.getNearbyEntityById(tokens[2])
+                    if mob != None:
+                        return Action(self.moveToMob, [mob])
+                elif isItem(entityType):
+                    item = self.getNearbyEntityById(tokens[2])
+                    if item != None:
+                        return Action(self.moveToItem, [item])
+                else:
+                    agent = HTNAgent.findAgentById(tokens[2])
+                    if agent != None:
+                        return Action(self.moveToAgent, [agent])
+                return None
+            elif tokens[0] == "!ATTACK":
+                entityType = "".join([i for i in tokens[2] if not i.isdigit()])
+                if isMob(entityType):
+                    mob = self.getNearbyEntityById(tokens[2])
+                    if mob != None:
+                        return Action(self.attackMob, [entity])
+                return None
+            elif tokens[0] == "!CRAFT":
+                ### TODO: In the log, we only output that we crafted an item, and do not include the recipe
+                ### Therefore, we do not know the recipe of items to pass to the craft function in order to craft the item given.
+                return None
+        elif len(tokens) == 4:  # COMMANDS LOGGED W/ 3 ARGUMENTS =======================================================
+            if tokens[0] == "!GIVEITEM":
+                itemType = "".join([i for i in tokens[2] if not i.isdigit()])
+                if isItem(itemType):
+                    item = self.inventory.itemById(self, tokens[2])
+                    receivingAgent = Agent.findAgentById(tokens[3])
+                    if item != None and receivingAgent != None:
+                        return Action(self.giveItemToAgent, [item, receivingAgent])
+                return None
+        return None
