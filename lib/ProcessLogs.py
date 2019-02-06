44f7bf56f72618a87da669614fab526bc67c24e3
Nathaniel Rex <nrex@icloud.com>
2019-02-06 16:59:17 -0500

    Modified post processing script to handle the requirement that new parameters in LOOKAT and MOVETO remain consistent after deleting parts of the log

--- lib/ProcessLogs.py
+++ lib/ProcessLogs.py
@@ -96,6 +96,9 @@ def addLine(line):
     for idx in range(0, len(strings)):
         # If an entity is referenced after already dying, do not add the line
         if endMarkerIndex == None and strings[idx] in dead_entities:
+            # Some actions reference a dead entity as the thing we are "looking FROM", "moving FROM", etc...
+            if line.startswith("!") and idx == 2 and len(strings) > 3:
+                continue
             return
     new_file_contents.append("-".join(strings))
 
@@ -181,7 +184,7 @@ def handleAttackLine(line, lineIdx):
         statusLine = getLine(targetAttackIdx + 1)
         if statusLine.startswith("status") and statusLine.endswith("dead") and attackedEntityId in statusLine.split("-"):
             new_file_contents.append(line)
-            new_file_contents.append(getLine(targetAttackIdx + 1))
+            handleEntityStatusLine(getLine(targetAttackIdx + 1))
             return lastAttackIdx - lineIdx if lastAttackIdx != targetAttackIdx else lastAttackIdx - lineIdx + 1
 
     # Attack was NOT conducted until completion. Loop backwards over new_file_contents and delete immediate prior actions on the attacked entity.
@@ -250,12 +253,16 @@ def checkActionPreconditions(idx, checkClosest=False):
                     if lineToCheck[k] != preconditions[j][k]:
                         allValuesMatch = False
 
-                if not allValuesMatch and preconditions[j][0].startswith("closest") and i < startMarkerIndex:   # ClosestXXX is a special case, where if we are checking for it, it can only appear in initial state
-                    # Modify closestXXX line in start state and remove precondition from before the action
-                    new_file_contents[i] = "-".join(preconditions[j])
+                # ClosestXXX is a special case, where if we are checking preconditions for the FIRST action, it must appear in initial state
+                if preconditions[j][0].startswith("closest") and checkClosest == True:
+                    if not allValuesMatch:  # Modify closestXXX in initial state to match
+                        new_file_contents[i] = "-".join(preconditions[j])
+                    
+                    # Remove closestXXX before action
                     del new_file_contents[preconditionLineNumbers[j]]
                     linesDeleted += 1
                     allValuesMatch = True
+
                 if not allValuesMatch:
                     return -1
                 del preconditions[j]
@@ -273,6 +280,7 @@ def checkActionPostconditions(idx):
     Returns the new length of new_file_contents.
     """
     global new_file_contents
+
     line = new_file_contents[idx]
     action = line.split("-")[0]
 
@@ -298,6 +306,39 @@ def checkActionPostconditions(idx):
     orignalLength = len(new_file_contents)
     del new_file_contents[startDeleteIdx:endDeleteIdx]
     return orignalLength - len(new_file_contents)
+
+def fixActionFromParameter(idx):
+    """
+    Given the line index of an action in the newly generated log contents, ensure that the parameter denoting where we last acted on is correct,
+    after having potentially deleted previous parts of the original log.
+    """
+    global new_file_contents
+
+    line = new_file_contents[idx]
+    strings = line.split("-")
+    statePrefix = None
+    entity = None
+    expectedParam = None
+    if line.startswith("!LOOKAT"):
+        statePrefix = "agent_looking_at"
+        entity = strings[1]
+        expectedParam = strings[2]
+    elif line.startswith("!MOVETO"):
+        statePrefix = "agent_at"
+        entity = strings[1]
+        expectedParam = strings[2]
+    else:
+        return  # Only applies to certain actions
+
+    # Loop upwards through the new log for the first appearance of the statePrefix, and ensure that the parameter in the action matches
+    for i in range(idx, -1, -1):
+        stringsToCheck = new_file_contents[i].split("-")
+        if stringsToCheck[0].startswith(statePrefix) and stringsToCheck[1] == entity:
+            actualParam = stringsToCheck[2]
+            if actualParam != expectedParam:
+                strings[2] = actualParam
+                new_file_contents[idx] = "-".join(strings)
+            return
 
 def processLogFile(filePath):
     """
@@ -385,10 +426,13 @@ def processLogFile(filePath):
             continue
         # If line represents an action
         elif strings[0] in ACTION_POST_TUPLES:
+            # If it is a LOOKAT or MOVETO command, possibly fix the 3rd parameter of where we looked/moved from previously after deleting parts of the log
+            fixActionFromParameter(lineIdx)
+
             # Check that each action's preconditions were actually set before-hand
             if nextActionIsFirstAction and startMarkerIndex != None:
                 nextActionIsFirstAction = False
-                returnValue = checkActionPreconditions(lineIdx, True)
+                returnValue = checkActionPreconditions(lineIdx, True)   # First action means 
             else:
                 returnValue = checkActionPreconditions(lineIdx)
             if returnValue < 0:
